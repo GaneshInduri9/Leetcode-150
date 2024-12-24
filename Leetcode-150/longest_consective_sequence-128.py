b18dc3a312e5922d5a5be7c417d3923148800732
Ganesh <ganeshInduri4@gmail.com>
2024-12-24 15:59:09 +0530

    Solved leetcode 128
    Signed-off-by: Ganesh <ganeshInduri4@gmail.com>

---
+++ Leetcode-150/longest_consective_sequence-128.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        maxLc = 0
+        for num in nums:
+            c = 0
+            while num+1 in nums:
+                c += 1
+                num = num + 1
+            maxLc = max(c, maxLc)
+        return maxLc
+    def longestConsecutiveOptimal(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        maxLc = 1
+        st = set(nums)
+        for num in nums:
+            c = 1
+            if num-1 not in st:
+                while num+1 in st:
+                    c += 1
+                    num = num + 1
+            maxLc = max(c, maxLc)
+        return maxLc
