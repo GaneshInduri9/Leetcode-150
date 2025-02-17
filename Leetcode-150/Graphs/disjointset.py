5ccb82582126677195aecc61a18bfaf5f6297b9a
Ganesh Induri <ganeshinduri4@gmail.com>
2025-02-17 19:21:56 +0530

    Solved union by size
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- Leetcode-150/Graphs/disjointset.py
+++ Leetcode-150/Graphs/disjointset.py
@@ -2,6 +2,7 @@ class DisjointSet:
     def __init__(self, n):
         self.rank = [0] * (n + 1)
         self.parent = [i for i in range(n + 1)]
+        self.size = [1 for i in range(n + 1)]
 
     def findParent(self, node):
         if node == self.parent[node]:
@@ -9,7 +10,7 @@ class DisjointSet:
         self.parent[node] = self.findParent(self.parent[node])
         return self.parent[node]
 
-    def union(self, u, v):
+    def unionByRank(self, u, v):
         ul_pu = self.findParent(u)
         ul_pv = self.findParent(v)
 
@@ -27,21 +28,35 @@ class DisjointSet:
             self.parent[ul_pv] = ul_pu
             self.rank[ul_pu] += 1
 
+    def unionBySize(self, u, v):
+        ul_pu = self.findParent(u)
+        ul_pv = self.findParent(v)
+
+        if ul_pu == ul_pv:
+            return
+
+        if self.size[ul_pu] < self.size[ul_pv]:
+            self.parent[ul_pu] = ul_pv
+            self.size[ul_pv] += self.size[ul_pu]
+        else:
+            self.parent[ul_pv] = ul_pu
+            self.size[ul_pu] += self.size[ul_pv]
+
 
 def test():
     d = DisjointSet(7)
-    d.union(1, 2)
-    d.union(2, 3)
-    d.union(4, 5)
-    d.union(6, 7)
-    d.union(5, 6)
+    d.unionByRank(1, 2)
+    d.unionByRank(2, 3)
+    d.unionByRank(4, 5)
+    d.unionByRank(6, 7)
+    d.unionByRank(5, 6)
 
     if d.findParent(3) == d.findParent(7):
         print("same brfore union")
     else:
         print("not same before union")
 
-    d.union(3, 7)
+    d.unionByRank(3, 7)
 
     if d.findParent(3) == d.findParent(7):
         print("same after union")
