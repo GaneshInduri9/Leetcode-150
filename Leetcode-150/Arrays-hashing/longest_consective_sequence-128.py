d3cf42f23f39b796b0b61f429c64a3612b9909f4
Ganesh Induri <ganeshinduri4@gmail.com>
2025-03-19 20:26:55 +0530

    Update leetcode 128
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- Leetcode-150/Arrays-hashing/longest_consective_sequence-128.py
+++ Leetcode-150/Arrays-hashing/longest_consective_sequence-128.py
@@ -1,14 +1,17 @@
 from typing import List
+
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         maxLc = 0
         for num in nums:
-            c = 0
-            while num+1 in nums:
+            c = 1
+            while num + 1 in nums:
                 c += 1
                 num = num + 1
             maxLc = max(c, maxLc)
         return maxLc
+
     def longestConsecutiveOptimal(self, nums: List[int]) -> int:
         if len(nums) == 0:
             return 0
@@ -16,9 +19,20 @@ class Solution:
         st = set(nums)
         for num in nums:
             c = 1
-            if num-1 not in st:
-                while num+1 in st:
+            if num - 1 not in st:
+                while num + 1 in st:
                     c += 1
                     num = num + 1
             maxLc = max(c, maxLc)
         return maxLc
+
+
+def test():
+    sol = Solution()
+    nums = [1, 6, 10, 2, 3, 4]
+    res = sol.longestConsecutive(nums)
+    print(res)
+
+
+if __name__ == "__main__":
+    test()
