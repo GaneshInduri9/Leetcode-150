1191cfe0f8e7206ad5e19bd852d4aa0b23e5ef81
Ganesh Induri <ganeshinduri4@gmail.com>
2025-03-23 15:27:51 +0530

    Search in rotated sorted array
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

---
+++ Leetcode-150/Binary-Search/searchinrotatedsorted.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+
+        l = 0
+        r = n - 1
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] >= nums[l]:
+                if target < nums[mid] and target >= nums[l]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+
+            else:
+                if target > nums[mid] and target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+
+
+def test():
+    sol = Solution()
+    print(sol.search([7, 8, 9, 1, 2, 3, 4, 5, 6], 1))
+
+
+if __name__ == "__main__":
+    test()
