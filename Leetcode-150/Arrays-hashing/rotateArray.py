0a800ec7aeecee5326d156126d301ffc0ce04350
Ganesh Induri <ganeshinduri4@gmail.com>
2025-03-25 20:10:59 +0530

    Array Problem
    Signed-off-by: Ganesh Induri <ganeshinduri4@gmail.com>

--- Leetcode-150/Arrays-hashing/rotateArray.py
+++ Leetcode-150/Arrays-hashing/rotateArray.py
@@ -1,2 +1,29 @@
+from typing import List
+
+
 class Solution:
-    def rotate(self, nums: list[int], k : int) -> None:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        if k > n:
+            k = k % n
+
+        def reverse(l: int, h: int):
+            while l < h:
+                nums[l], nums[h] = nums[h], nums[l]
+                l += 1
+                h -= 1
+
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
+
+
+def test():
+    sol = Solution()
+    nums = [1, 2, 3, 4, 5, 6, 7]
+    sol.rotate(nums, 3)
+    print(nums)
+
+
+if __name__ == "__main__":
+    test()
